fa84666176d0b2df5c3962045c37780116b4089e
Kh-Ol-An <olegkhristenko@gmail.com>
2020-03-19 16:35:38 +0200

    fix

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const input = document.querySelector(".js-input");
 const createBtn = document.querySelector('button[data-action="create"]');
 const destroyBtn = document.querySelector('button[data-action="destroy"]');
 const boxes = document.querySelector("#boxes");
-let divs = [];
 
 boxes.style.display = "flex";
 boxes.style.flexFlow = "wrap";
@@ -15,11 +14,9 @@ const randomInteger = (min, max) => {
 };
 
 const createBoxes = amount => {
-  for (let i = 0; i < amount; i += 1) {
-    divs.push(document.createElement("div"));
-  }
   let px = 0;
-  divs.forEach(div => {
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement("div");
     div.style.width = `${30 + px}px`;
     div.style.height = `${30 + px}px`;
     div.style.background = `rgb(${randomInteger(0, 255)},
@@ -27,7 +24,7 @@ const createBoxes = amount => {
     ${randomInteger(0, 255)})`;
     boxes.insertAdjacentElement("beforeend", div);
     px += 10;
-  });
+  }
 };
 
 const handleCreate = () => {
@@ -36,8 +33,7 @@ const handleCreate = () => {
 createBtn.addEventListener("click", handleCreate);
 
 const destroyBoxes = () => {
-  divs.forEach(div => boxes.removeChild(div));
-  divs = [];
+  boxes.querySelectorAll("*").forEach(item => item.remove());
   input.value = null;
 };
 destroyBtn.addEventListener("click", destroyBoxes);
